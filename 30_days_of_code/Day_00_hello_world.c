cf3ee34c0f7d711d72e3d517fc2fff471600bd56
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-28 19:03:07 +0200

    udpate 30 days of code day 00

--- 30_days_of_code/Day_00_hello_world.c
+++ 30_days_of_code/Day_00_hello_world.c
@@ -4,29 +4,17 @@
 #include <stdlib.h>
 
 int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[105];
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        char input_string[1000];
-        scanf(" %[^\n]", input_string);
-        for (int j = 0; j < strlen(input_string); j++) {
-            if (j % 2 == 0) {
-                printf("%c", input_string[j]);
-            }
-        }
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    scanf("%[^\n]", input_string);
 
-        printf(" ");
+    // Print a string literal saying "Hello, World." to stdout using printf.
+    printf("Hello, World.\n");
+    printf("%s", input_string);
 
-        for (int j = 0; j < strlen(input_string); j++) {
-            if (j % 2 != 0) {
-                printf("%c", input_string[j]);
-            }
-        }
-
-        printf("\n");
-    }
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
 
     return 0;
 }
