0124b3d6bb53c660f74c0d04e01bb2194f475047
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-28 20:10:17 +0200

    update 30 days of code 08 in python

--- 30_days_of_code/Day_08_dictionaries_and_maps.py
+++ 30_days_of_code/Day_08_dictionaries_and_maps.py
@@ -1,15 +1,32 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+def solucion_01():
+    n = int(input())
+    phone_book = {}
+
+    for _ in range(n):
+        name, phone_number = input().split()
+        phone_book[name] = int(phone_number)
+
+    while True:
+        try:
+            name = input()
+            print(f"Not found" if name not in phone_book else f"{name}={phone_book[name]}")
+        except EOFError:
+            break
+
+def solucion_02():
 n = int(input())
 phone_book = {}
 
 for _ in range(n):
     name, phone_number = input().split()
-    phone_book[name] = int(phone_number)
+    phone_book[name] = phone_number
 
-for _ in range(n):
-    name = input()
-
-    if name not in phone_book:
-        print("Not found")
-    else:
+while True:
+    try:
+        name = input()
         print(f"{name}={phone_book[name]}")
+    except KeyError:
+        print("Not found")
+    except EOFError:
+        break
