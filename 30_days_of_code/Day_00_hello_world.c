958a61cec38777655c6b1e3dfe40d88182537837
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-28 18:41:47 +0200

    add 30 days of code day 00

---
+++ 30_days_of_code/Day_00_hello_world.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        char input_string[1000];
+        scanf(" %[^\n]", input_string);
+        for (int j = 0; j < strlen(input_string); j++) {
+            if (j % 2 == 0) {
+                printf("%c", input_string[j]);
+            }
+        }
+
+        printf(" ");
+
+        for (int j = 0; j < strlen(input_string); j++) {
+            if (j % 2 != 0) {
+                printf("%c", input_string[j]);
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
